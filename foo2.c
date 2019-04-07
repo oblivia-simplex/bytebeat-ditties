8507fd3d1bd8db48f4900c478eb83a688ccc943e
Oblivia Simplex <oblivia@paranoici.org>
2019-04-07 16:07:15 -0300

    simplified expression a little

--- foo2.c
+++ foo2.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-
+/* sample rate: 8000 Hz. Unsigned 8 bit. 
+ * compile with `gcc foo2.c -o foo`
+ * play with `./foo2 | aplay -r 8000`
+ */
 int main(int t) {
-  for (;;t++) {
+  for (t=0;;t++) {
     putchar(
         t 
-        & t>>6
-        & ((t%(1<<14))<(1<<13)? (t^t>>8):(t>>8)) 
-        / (1+!t + (t%32)) 
-        ^ ((t%30)) 
-        | (((t%(1<<3)>(t<<8)? t%3 : t/10)))
+        & t >> 6
+        & ((t % (1 << 14)) < (1 << 13)? 
+           t ^ t >> 8
+           : t >> 8) 
+        / (1 + t % 32) 
+        ^ (t % 30) 
+        | t / 10
     );
+    fprintf(stderr, "%x\n", t);
   }
   return 0;
 }
